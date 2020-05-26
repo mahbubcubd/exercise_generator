1c2550b6addd649b1d48ef40dd0a31f6cd5799c3
Mahbubur Rahman <mahbubcu14@gmail.com>
2020-05-26 20:57:01 +0600

    Minor modifications

--- find_exercise.py
+++ find_exercise.py
@@ -19,13 +19,12 @@ def take_int_list_input(category_list, catergory_id_list, query_word):
 
         #Print instructions for category
         print("---------------------------------------------------------------------")
-        print("Please write the "+ query_word + " numbers. You can choose multiple " + query_word + " at a time. Separate them with COMMA(,)")
         shuffle(catergory_id_list)
         
-        print("For example: " + str(catergory_id_list[0:3])[1:-1])
+        #print("For example: " + str(catergory_id_list[0:3])[1:-1])
 
         #Ask for category input from user
-        user_category_input = input("Select " + query_word + "\nnumber from the above list\nor press ENTER to skip:\n")
+        user_category_input = input("Choose " + query_word + " number from the above list. Put COMMA(,) for multiple selection. \nYou can press ENTER to skip: ")
         #print(user_category_input)
         
         #Check whether user gave an empty input
@@ -50,29 +49,15 @@ def take_int_list_input(category_list, catergory_id_list, query_word):
                 category_input_list = list(category_input_list-elements_to_remove)
             else:
                 pass
+            break_category_loop = True
 
         except Exception as e:
-            messeage = 'Enter valid '+ query_word +' numbers using comma seperation\n\n' + 'Exception : ' + str(e) + '\noccured while handling your input' 
+            messeage = 'Enter valid '+ query_word +' numbers using comma seperation\n' 
+            # 'Exception : ' + str(e) + '\noccured while handling your input' 
             print()
             print()
             print(messeage)
 
-
-        
-        if (len(category_input_list) == 0) and (is_input_given):
-            print("\nYou have not entered any valid "+query_word+"\nPlease enter a valid "+query_word+"!")
-            print("Press ENTER to continue")
-            input()
-
-        elif (len(category_input_list) == 0) and (not is_input_given):
-            print()
-            print("You choose not a give a "+ query_word + " input")
-            break_category_loop = True
-
-        else:
-            break_category_loop = True
-            
-        #--------------------------------------------#
     return category_input_list, is_input_given
 
 
@@ -94,7 +79,7 @@ def take_int_input(query_word):
                 number = int(number_string)
                 break_input_loop = True
             else:
-                number = 10000
+                number = 0
                 break_input_loop = True
             
         except Exception as e:
@@ -107,8 +92,8 @@ def take_int_input(query_word):
 
 
 
-def print_list(my_list,max_output_number):        
-    for i,item in enumerate(my_list[:max_output_number]):
+def print_list(my_list):        
+    for i,item in enumerate(my_list):
         print("{:02d}".format( i+1 ), '|  ', item)
         
 
@@ -160,7 +145,7 @@ if __name__ == '__main__':
     #Take exercise category input from user
     exercise_input_list, _ = take_int_list_input(category_list, catergory_id_list,'exercise category')
 
-    print(exercise_input_list)
+    # print(exercise_input_list)
 
     print()
     print("Press ENTER to continue")
@@ -169,25 +154,13 @@ if __name__ == '__main__':
     #Take muscle category input from user
     muscle_input_list, _ = take_int_list_input(muscle_list, muscle_id_list, 'muscle category')
     
-    print(muscle_input_list)
+    # print(muscle_input_list)
 
 
     print()
-    print("Press ENTER to continue")
-    input()
 
     #Ask user to give maximum number of output 
     max_output_number = take_int_input("maximum number of exercises to show\nor press ENTER to skip:\n")
-    if max_output_number < 10000:
-        if max_output_number>1:
-            print("You will be shown maximum "+ str(max_output_number) + " exercises\n\n")
-        else:
-            print("You will be shown maximum "+ str(max_output_number) + " exercise\n\n")
-    else:
-        print("You will be shown all exercises \nthat meet your search criteria\n\n")
-
-    print("Press ENTER to continue")
-    input()
     
 
     # Find out the exercises to show as output
@@ -207,23 +180,30 @@ if __name__ == '__main__':
             output_exercise_list.append(exercise['fields']['name'])
             #print('Added', exercise['fields']['name'],' to output list')
     
-    #bodyweight=input("Number of bodyweight exercises from[0-{}] =  ".format#(str(len(bodyweight_dict))) )
+    # bodyweight=input("Number of bodyweight exercises from[0-{}] =  ".format#(str(len(bodyweight_dict))) )
 
-    bodyweight = take_int_input( "number of bodyweight exercises from[0-{}] =  ".format(str(len(bodyweight_dict))) + "\n")
+    bodyweight = take_int_input( "number of bodyweight exercises from[0-{}] :   ".format(str(len(bodyweight_dict))) )
 
     try:
         tn=int(bodyweight)
         shuffle(bodyweight_dict)
-        bdwt=bodyweight_dict[0:tn]
+        if tn>=0:
+            bdwt=bodyweight_dict[0:tn]
+        else:
+            bdwt=[]
     except Exception as e:
         print("Error ", e)
         bdwt=[]
     
+    try:
+        output_exercise_list=output_exercise_list[0:max_output_number]
+    except:
+        print()
     output_exercise_list.extend(bdwt)
     # Print output 
     print("Here is your list of exercises")
     print()
-    print_list(output_exercise_list, max_output_number)
+    print_list(output_exercise_list)
 
 
 
